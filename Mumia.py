f74346372ca645bd380da015a3040b1266838198
Dmitriy <zdmitrij506@gmail.com>
2024-01-20 10:37:15 +0300

    sd

--- Mumia.py
+++ Mumia.py
@@ -1,24 +1,34 @@
-import pygame.sprite
+import pygame
+from pygame.transform import scale
+import math
 
 
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, coords):
-        super().__init__()
-        self.image = pygame.image.load(f"images/tiles/Tile2.png")
-        self.rect = self.image.get_rect()
-        self.rect.topleft = coords
+class mymia(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__()
+        self.image = pygame.image.load(f"image/Мумия.png")
+        self.original_image = pygame.image.load(f"image/Мумия.png")
+        self.rect = pygame.Rect(x, y, 150, 150)
         self.xvel = 0
         self.yvel = 0
+        self.life = 5
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
-    def update(self, rectplayer):
-        if rectplayer.y:
-            self.xvel -= 1
-        if right:
-            self.xvel += 1
-        if forward:
-            self.yvel += 1
-        if back:
+    def update(self, rectplayer, bulletrect):
+        if rectplayer.y < self.rect.y:
             self.yvel -= 1
+        elif rectplayer.y > self.rect.y:
+            self.yvel += 1
+        elif rectplayer.x < self.rect.x:
+            self.xvel -= 1
+        elif rectplayer.x > self.rect.x:
+            self.xvel += 1
+        else:
+            pass
+        mouse_x, mouse_y = rectplayer.center
+        rel_x, rel_y = mouse_x - self.rect.x, mouse_y - self.rect.y
+        angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
+        self.image = pygame.transform.rotate(self.original_image, int(angle))
+        self.rect = self.image.get_rect(center=self.rect.center)
